7118309b6ca338b4b99409cefadd1e2c28af68a2
Истомина Елена <69istominaea@region.cbr.ru>
2026-05-03 11:21:18 +0300

    fix unused import

--- src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/FilmService.java
@@ -20,7 +20,6 @@ import ru.yandex.practicum.filmorate.storage.db.LikeDbStorage;
 import ru.yandex.practicum.filmorate.storage.db.MpaDbStorage;
 import ru.yandex.practicum.filmorate.validators.FilmValidator;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
