688aee4aa363d91f2224d3d26a26f0052462d985
Истомина Елена <69istominaea@region.cbr.ru>
2026-05-11 10:15:53 +0300

    fix checks

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/ReviewMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/ReviewMapper.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.yandex.practicum.filmorate.dto.ReviewDto;
 import ru.yandex.practicum.filmorate.model.Review;
 
