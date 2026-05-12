2e372d9048589a8e732b692f4c4ed7a7ee52020b
Истомина Елена <69istominaea@region.cbr.ru>
2026-05-12 18:36:01 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -21,7 +21,11 @@ public class Review {
     private LocalDateTime createdAt;
     private Boolean isPositive;
     private Integer useful;
+    @NotNull
+    @NotBlank
     private Long filmId;
+    @NotNull
+    @NotBlank
     private Long userId;
     private Integer likesCount;
     private Integer dislikesCount;
